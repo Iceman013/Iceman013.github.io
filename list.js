dc3192570abe11bb7771754299afed3d9103d3c6
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2021-10-06 16:50:01 -0400

    Update list.js

--- list.js
+++ list.js
@@ -1,31 +1,74 @@
 var apartments = [];
-var a = 0;
-apartments.push(new Apartment("a", "Worst Western", "www.here.com", "Big place"));
-apartments.push(new Apartment("b", "La Keenta", "www.there.com", "Tiny place"));
-apartments.push(new Apartment("c", "Holiday Out", "www.este.com", "Broken down"));
-apartments.push(new Apartment("d", "Mary Ott", "www.coolmath.com", "Affordable"));
-apartments.push(new Apartment("e", "Other fake name", "www.website?.com", "Made of baby tears"));
-function Apartment(id, name, link, description) {
+apartments.push(new Apartment("a", "Worst Western", "www.here.com", "Big place", {lat: 33.9598, lng: -83.370}, false));
+apartments.push(new Apartment("b", "La Keenta", "www.there.com", "Tiny place", {lat: 33.9598, lng: -83.371}, false));
+apartments.push(new Apartment("c", "Holiday Out", "www.este.com", "Broken down", {lat: 33.9598, lng: -83.372}, false));
+apartments.push(new Apartment("d", "Mary Ott", "www.coolmath.com", "Affordable", {lat: 33.9598, lng: -83.373}, false));
+apartments.push(new Apartment("e", "Fake name", "www.website?.com", "For the rich", {lat: 33.9598, lng: -83.374}, false));
+function Apartment(id, name, link, description, position, onMap) {
   this.id = id;
   this.name = name;
   this.link = link;
   this.description = description;
+  this.position = position;
+  this.onMap = onMap;
+}
+function loadList() {
+  var a = 0;
+  while (a < apartments.length) {
+    createApartment(a);
+    a = a + 1;
+  }
 }
 function function1() {
+  var a = 0;
+  if (a == 0) {
+    displayApartment(false, 1);
+  }
+  if (a == 1) {
+    displayApartment(false, 2);
+  }
+  if (a == 2) {
+    displayApartment(true, 1);
+  }
+  a = a + 1;
+}
+function showMap(input) {
+  apartments[input].onMap = !apartments[input].onMap;
+  var a = 0;
+  var mList = [];
+  while (a < apartments.length) {
+    if (apartments[a].onMap) {
+      mList.push(apartments[a].position);
+    }
+    a = a + 1;
+  }
+  initMap(mList);
+}
+function displayApartment(boolean, number) {
+  if (boolean) {
+    document.getElementById(apartments[number].id).style.display = "block";
+  } else {
+    document.getElementById(apartments[number].id).style.display = "none";
+  }
+}
+function createApartment(number) {
   var list = document.getElementById("list");
   
   var divElement = document.createElement("div");
   
-  divElement.id = apartments[a].id;
+  divElement.id = apartments[number].id;
   divElement.classList.add("detail");
   
   item = document.createElement("input");
   item.type = "checkbox";
   item.classList.add("check");
+  item.addEventListener("click", function() {
+    showMap(number);
+  });
   divElement.appendChild(item);
   
   item = document.createElement("button");
-  item.innerHTML = " " + apartments[a].name;
+  item.innerHTML = apartments[number].name;
   item.type = "button";
   item.classList.add("name");
   item.addEventListener("click", function() {
@@ -44,16 +87,15 @@ function function1() {
   content.classList.add("content");
   
   item = document.createElement("a");
-  item.innerHTML = apartments[a].link;
-  item.href = apartments[a].link;
+  item.innerHTML = apartments[number].link;
+  item.href = apartments[number].link;
   
   content.appendChild(item);
   
   item = document.createElement("p");
-  item.innerHTML = apartments[a].description;
+  item.innerHTML = apartments[number].description;
   
   content.appendChild(item);
   divElement.appendChild(content);
   list.appendChild(divElement);
-  a = a + 1;
 }
