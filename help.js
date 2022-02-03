1aa2355b29b8dbe9ea55c46279f5e10a7d30c736
Bret Jones <bretcj1@gmail.com>
2022-02-03 17:54:37 -0500

    Made an icon and a menu

--- help.js
+++ help.js
@@ -2,7 +2,7 @@ function showPlans() {
     var file = document.createElement("embed");
     file.src = "plans.md";
     file.type = "text/markdown";
-    file.height = "600px";
+    file.style.overflow = "hidden";
     file.width = "100%";
     makeModal("Plans", file);
     openModal();
