e0937b14604338187ed2921517bc94544b1b2e11
Bret Jones <bretcj1@gmail.com>
2022-02-02 19:33:13 -0500

    Slight style changes

--- help.js
+++ help.js
@@ -2,7 +2,7 @@ function showPlans() {
     var file = document.createElement("embed");
     file.src = "plans.md";
     file.type = "text/markdown";
-    file.height = "700px";
+    file.height = "600px";
     file.width = "100%";
     makeModal("Plans", file);
     openModal();
