ba6ace38b1edb7221b4db750230d80cf42a5c39c
Graham Tokarz <32685880+Monkeytoes999@users.noreply.github.com>
2022-03-02 09:44:10 -0500

    Added a garm answer

--- answerList.js
+++ answerList.js
@@ -3,6 +3,7 @@ const answers = [
     new Answer("Unicycle", "125.99"),
     new Answer("Dinosaur", "867.53"),
     new Answer("Pickles", "4.19"),
+    new Answer("Pretzel", "3.59"),
     new Answer("Ramen", "2.99"),
     new Answer("Dunkaccino","2.09")
 ];
