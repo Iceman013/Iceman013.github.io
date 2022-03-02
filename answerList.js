874967ccffcaf38051d3291ae2cdd0968a2ba943
Bret Jones <bretcj1@gmail.com>
2022-03-01 21:22:13 -0500

    Daily updater

--- answerList.js
+++ answerList.js
@@ -1,6 +1,7 @@
 const mp = 6;
 const answers = [
     new Answer("Unicycle", "125.99"),
+    new Answer("Dinosaur", "867.53"),
     new Answer("Pickles", "4.19"),
     new Answer("Ramen", "2.99")
 ];
