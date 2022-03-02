83d667c803b6df0c6437a7307a5e4e9ea467e032
Bret Jones <bretcj1@gmail.com>
2022-03-02 14:29:07 -0500

    Crosshair

--- flight/main.js
+++ flight/main.js
@@ -1,6 +1,11 @@
-var angle = 0;
+var xPos = 0;
 const TICK = 10;
+const SPEED = 30;
 var buttons = {};
+var mouse = {
+    "x": 0,
+    "y": 0
+};
 const update = new Event("update");
 window.addEventListener("keydown", function(event) {
     buttons[event.key] = true;
@@ -10,16 +15,28 @@ window.addEventListener("keyup", function(event) {
     buttons[event.key] = false;
     window.dispatchEvent(update);
 });
+window.addEventListener("mousemove", function(event) {
+    mouse["x"] = event.clientX;
+    mouse["y"] = event.clientY;
+});
 function getState() {
-    if (buttons["q"]) {
-        angle++;
+    if (buttons["a"]) {
+        xPos += SPEED;
     }
-    if (buttons["e"]) {
-        angle--;
+    if (buttons["d"]) {
+        xPos -= SPEED;
+    }
+    if (xPos > 5600) {
+        xPos = 5600;
+    }
+    if (xPos < -4700) {
+        xPos = -4700;
     }
 }
 function display() {
-    this.document.getElementById("background").style.transform = "rotate(" + angle + "deg)";
+    this.document.getElementById("background").style.transform = "translate(" + xPos + "px)";
+    var temp = "translate(" + (mouse["x"] - 100) + "px, " + (mouse["y"] - 100) + "px)";
+    this.document.getElementById("cross").style.transform = temp;
 }
 var intervalId = window.setInterval(function() {
     tick();
