41c08d659db084fb9ddb826b24a3176942d3240e
Bret Jones <bretcj1@gmail.com>
2022-03-02 23:54:44 -0500

    PLEASE WORK

--- main/siteList.js
+++ main/siteList.js
@@ -1,4 +1,4 @@
 const siteList = [
     new Site("Retaildle", "Games", "https://iceman013.github.io/retaildle/"),
-    //new Site("Paint Bot", "Games", "https://iceman013.github.io/paintBot/")
+    new Site("Paint Bot", "Games", "https://iceman013.github.io/paintBot/")
 ];
