07d0b2154135cb949a562cbec883a0867f22a375
Bret Jones <bretcj1@gmail.com>
2022-03-02 19:01:52 -0500

    This will do

--- main/siteList.js
+++ main/siteList.js
@@ -1,7 +1,3 @@
 const siteList = [
-    new Site("Retaildle", "Game", "https://iceman013.github.io/retaildle/"),
-    new Site("Retaildleb", "Game", "https://iceman013.github.io/retaildle/"),
-    new Site("Retaildlec", "Game", "https://iceman013.github.io/retaildle/"),
-    new Site("Retaildled", "Other", "https://iceman013.github.io/retaildle/"),
-    new Site("Retaildlee", "Other", "https://iceman013.github.io/retaildle/")
+    new Site("Retaildle", "Games", "https://iceman013.github.io/retaildle/"),
 ];
