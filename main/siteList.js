6ff7cd665171357c332969f9abc025e01e64afc9
Bret Jones <bretcj1@gmail.com>
2022-03-10 16:42:29 -0500

    Color palete'

--- main/siteList.js
+++ main/siteList.js
@@ -1,4 +1,6 @@
 const siteList = [
+    new Site("Paint Bot", "Games", "https://iceman013.github.io/paintBot/"),
     new Site("Retaildle", "Games", "https://iceman013.github.io/retaildle/"),
-    new Site("Paint Bot", "Games", "https://iceman013.github.io/paintBot/")
+    new Site("Retail Mini", "Games", "https://iceman013.github.io/paintBot/"),
+    new Site("Demo", "Unfinished", "https://iceman013.github.io/demo/")
 ];
