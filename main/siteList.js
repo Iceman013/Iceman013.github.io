e88d2328c224bb33865ace879d59c84555af7153
Bret Jones <bretcj1@gmail.com>
2022-03-12 11:46:04 -0500

    adding site? pffft

--- main/siteList.js
+++ main/siteList.js
@@ -1,7 +1,7 @@
 const siteList = [
     new Site("Paint Bot", "Games", "https://iceman013.github.io/paintBot/"),
     new Site("Retaildle", "Games", "https://iceman013.github.io/retaildle/"),
-    new Site("Retail Mini", "Games", "https://iceman013.github.io/paintBot/"),
+    new Site("Retail Mini", "Games", "https://iceman013.github.io/retailMini/"),
 
     new Site("Demo", "Unfinished", "https://iceman013.github.io/demo/"),
 
