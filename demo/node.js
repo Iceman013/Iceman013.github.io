28ac0295d4e67d1965baa11bf7cf03d23df92180
Bret Jones <bretcj1@gmail.com>
2022-03-15 16:46:07 -0400

    Maybe later

--- demo/node.js
+++ demo/node.js
@@ -1,9 +1,21 @@
 const SHRINK = 0.9;
+var SIZE = {
+    "Minimum": 100,
+    "Maximum": 200
+};
+const DEPTH = 6;
 function Node() {
     this.children = [];
+    this.maxChildren = 3;
 
+    this.canAdd = function() {
+        out = this.children.length < this.maxChildren;
+        return out;
+    }
     this.addNode = function(newChild) {
-        this.children.push(newChild);
+        if (this.canAdd()) {
+            this.children.push(newChild);
+        }
     }
     this.getDisplay = function() {
         var out = "[";
@@ -25,17 +37,37 @@ function Node() {
         }
         return base;
     }
+
+    this.totalChildren = function() {
+        var sum = 1;
+        for (let i = 0; i < this.children.length; i++) {
+            sum += this.children[i].totalChildren();
+        }
+        return sum;
+    }
 }
-function genNew(chance) {
+function getProbability(iteration) {
+    if (iteration < DEPTH) {
+        return Math.pow(SHRINK, iteration);
+    } else {
+        return 0;
+    }
+}
+function genNew(round) {
     var y = new Node();
     var r = Math.random();
-    while (r < SHRINK*chance) {
-        var q = genNew(SHRINK*chance);
+    while (r < getProbability(round) && y.canAdd()) {
+        var q = genNew(round + 1);
         y.addNode(q);
         r = Math.random();
     }
     return y;
 }
 function newNode() {
-    return genNew(1);
+    var out = genNew(1);
+    while (out.totalChildren() >  SIZE["Maximum"] || out.totalChildren() < SIZE["Minimum"]) {
+        out = genNew(1);
+    }
+    console.log(out.totalChildren());
+    return out;
 }
