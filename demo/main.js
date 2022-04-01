4762db92f3731a9d01258610b5305372e3f657db
Bret Jones <bretcj1@gmail.com>
2022-04-01 10:48:52 -0400

    Try console fun stuff.

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,19 @@
 function start() {
-    var d = newNode();
-    console.log(d.getDisplay());
-    document.getElementById("base").appendChild(d.getBox());
+    console.clear();
+    console.time();
+    console.log("Begining logs");
+    console.error("Error message");
+    console.group("Group");
+    console.log("%cABC DEF %cGHI", "background: green", "background: red");
+    console.log("B");
+    console.groupEnd();
+    console.groupCollapsed("OwO");
+    console.log("A");
+    console.log("B");
+    console.groupEnd();
+    console.table([['red',4],['blue',8],['green',3]]);
+    console.warn("Warning message");
+    console.trace();
+    console.timeEnd();
 }
 start();
