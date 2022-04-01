c46401028be9bd4d00cd752a26006e7195a73e46
Bret Jones <bretcj1@gmail.com>
2022-04-01 11:10:12 -0400

    Fun read

--- demo/main.js
+++ demo/main.js
@@ -2,10 +2,15 @@ function start() {
     console.clear();
     console.time();
     console.log("Begining logs");
+    console.log("%cThis is how %ccolors work", "background: green",
+    "background: red; color: yellow; font-size: 30px; text-shadow: 2px 4px orange; font-weight: 900")
     console.error("Error message");
+    console.group("%cThe fun will never end", "font-size: 22px;");
+    console.log("Oh my");
     console.group("Group");
-    console.log("%cABC DEF %cGHI", "background: green", "background: red");
+    console.log("A");
     console.log("B");
+    console.groupEnd();
     console.groupEnd();
     console.groupCollapsed("OwO");
     console.log("A");
