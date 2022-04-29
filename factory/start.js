071dccb93d0f21063fdd91bbeb8a6cc012350047
Bret Jones <bretcj1@gmail.com>
2022-04-28 20:59:04 -0400

    Artistic passion

--- factory/start.js
+++ factory/start.js
@@ -21,31 +21,61 @@ function newCells() {
     map.style.left = (position.pxoff) + "px";
     map.style.top = (position.pyoff) + "px";
 
-    for (let i = -1; i < SWIDTH; i++) {
-        for (let j = -1; j < SHEIGHT; j++) {
+    for (let i = 0; i <= SWIDTH; i++) {
+        for (let j = 0; j <= SHEIGHT; j++) {
             var base = document.createElement("img");
-            base.style.left = 32*i + "px";
-            base.style.top = 32*j + "px";
+            base.src = "images/DefaultGround.png";
+            base.style.left = (32*i - 32) + "px";
+            base.style.top = (32*j - 32) + "px";
             map.appendChild(base);
         }
     }
 }
 function move() {
-    if (keys["w"]) {
-        position.pyoff++;
+    var velocity = 2;
+    var dividen = 0;
+    if (keys.w) {
+        dividen++;
     }
-    if (keys["a"]) {
-        position.pxoff++;
+    if (keys.a) {
+        dividen++;
     }
-    if (keys["s"]) {
-        position.pyoff--;
+    if (keys.s) {
+        dividen++;
     }
-    if (keys["d"]) {
-        position.pxoff--;
+    if (keys.d) {
+        dividen++;
+    }
+    if (dividen > 1) {
+        velocity = Math.SQRT1_2*velocity;
+    }
+    if (keys["w"] && position.mapy < HEIGHT - SHEIGHT) {
+        position.pyoff += velocity;
+    }
+    if (keys["a"] && position.mapx < WIDTH - SWIDTH) {
+        position.pxoff += velocity;
+    }
+    if (keys["s"] && position.mapy > 0) {
+        position.pyoff -= velocity;
+    }
+    if (keys["d"] && position.mapx > 0) {
+        position.pxoff -= velocity;
     }
 }
 function moveUpdate() {
     if (position.pxoff >= 32 || position.pyoff >= 32 || position.pxoff < 0 || position.pyoff < 0) {
+        if (position.pxoff >= 32) {
+            position.mapx += 1;
+        }
+        if (position.pxoff < 0) {
+            position.mapx -= 1;
+        }
+        if (position.pyoff >= 32) {
+            position.mapy += 1;
+        }
+        if (position.pyoff < 0) {
+            position.mapy -= 1;
+        }
         position.pxoff = (32*100 + position.pxoff) % 32;
         position.pyoff = (32*100 + position.pyoff) % 32;
         newCells();
