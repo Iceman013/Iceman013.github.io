c747ed09052535592dcf8a46771dea4f8800f3d4
Bret Jones <bretcj1@gmail.com>
2022-07-30 00:17:19 -0400

    Art

--- kostimo/jamlist.js
+++ kostimo/jamlist.js
@@ -1,5 +1,14 @@
 const jamList = [
     new Jam("incDoot.mp3"),
     new Jam("chem.mp3"),
-    new Jam("snok.mp3")
+    new Jam("snok.mp3"),
+    new Jam("big.mp3"),
+    new Jam("fire.mp3"),
+    new Jam("home.mp3"),
+    new Jam("jimmy.mp3"),
+    new Jam("ow.mp3"),
+    new Jam("school.mp3"),
+    new Jam("stuck.mp3"),
+    new Jam("toenails.mp3"),
+    new Jam("yeah.mp3")
 ];
--- kostimo/main.js
+++ kostimo/main.js
@@ -2,14 +2,24 @@ function makeAudios() {
     for (let i = 0; i < jamList.length; i++) {
         var base = document.createElement("audio");
         base.id = "ad" + i;
-        base.loop = "true";
         
         var jim = document.createElement("source");
-        jim.src = jamList[i].file;
+        jim.src = "sounds/" + jamList[i].file;
         jim.type = "audio/mpeg";
 
         base.appendChild(jim);
         document.getElementById("musics").appendChild(base);
     }
 }
+function makeButtons() {
+    for (let i = 0; i < jamList.length; i++) {
+        var clack = document.createElement("button");
+        clack.onclick = function() {
+            document.getElementById("ad" + i).play();
+        };
+        clack.innerHTML = "This";
+        document.getElementById("trys").appendChild(clack);
+    }
+}
 makeAudios();
+makeButtons();
