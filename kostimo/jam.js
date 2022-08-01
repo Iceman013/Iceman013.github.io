3d1ecf8d574061c8311c28b2af9a9093ad60cf14
Bret Jones <bretcj1@gmail.com>
2022-08-01 01:46:12 -0400

    Internal id

--- kostimo/jam.js
+++ kostimo/jam.js
@@ -1,7 +1,10 @@
+var internalID = 0;
 function Jam(name, file, volume, type, tag) {
     this.name = name;
     this.file = file;
     this.volume = volume;
     this.type = type;
     this.tag = tag;
+    this.id = internalID;
+    internalID++;
 }
