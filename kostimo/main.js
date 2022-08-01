9ff4c6e021021d4a106ad8f57338a222b9d907c8
Monkeytoes999 <monkeytoes999@gmail.com>
2022-08-01 00:21:09 -0400

    Looping visuals and better stopping

--- kostimo/main.js
+++ kostimo/main.js
@@ -1,9 +1,20 @@
 var playing = false;
+var pval = 0;
+var lit = [];
 
 function stateChange(){
     playing = !playing;
     if (playing) {
         mainloop();
+        document.getElementById("egg").innerHTML = "Stop"
+    } else {
+        document.getElementById("egg").innerHTML = "Please Wait"
+        document.getElementById("egg").disabled = true;
+        pval = 0;
+        while (lit.length > 0) {
+            document.getElementById("bli(" + lit[0][0] + "," + lit[0][1] + ")").style.filter = "opacity(1)";
+            lit.shift();
+        }
     }
 }
 
@@ -13,13 +24,31 @@ function mainloop() {
         setTimeout(() => {
             eid = "ts" + (j+1);
             j++;
-            for (k=0; k<tracks; k++){
-                if (document.getElementById("che(" + k + "," +  j + ")").checked) document.getElementById("aud(" + k + "," +  j + ")").play();
+            console.log(pval, j)
+            if (pval == j){
+                for (k=0; k<tracks; k++){
+                    if (document.getElementById("but" + k).value != -1) {                    
+                        document.getElementById("bli(" + k + "," + j + ")").style.filter = "opacity(.85)";
+                        lit.push([k,j])
+                        setTimeout(() => {
+                            document.getElementById("bli(" + lit[0][0] + "," + lit[0][1] + ")").style.filter = "opacity(1)";
+                            lit.shift();
+                        }, 250);
+                        if (document.getElementById("che(" + k + "," +  j + ")").checked) document.getElementById("aud(" + k + "," +  j + ")").play();
+                    }
+                }
+                pval++;
+                if (pval >= loopDur*frequency) pval = 0;
             }
         }, i*250);
     }
     setTimeout(() => {
-        if (playing) mainloop();
+        if (playing) { 
+            mainloop() 
+        } else {
+            document.getElementById("egg").innerHTML = "Play";
+            document.getElementById("egg").disabled = false;
+        }
     }, loopDur*1000);
 }
 
