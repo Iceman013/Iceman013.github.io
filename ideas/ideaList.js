b21d59a09cddbb6007e64bbcd3e9d1b100870b33
Bret Jones <bretcj1@gmail.com>
2022-08-05 14:49:08 -0400

    New idea

--- ideas/ideaList.js
+++ ideas/ideaList.js
@@ -13,8 +13,14 @@
 
 const ideaList = [
     new Idea("Marmalade", ["Java"], ["Utility", "New"], "A app version of Syrup that has startup features.", false),
+
     new Idea("MapGen", ["HTML/JS"], ["Game", "Repeat"], "A game that generates a map that stuff can be built on.", false),
+
     new Idea("Idle Game", ["HTML/JS"], ["Game", "Repeat"], "Any idle game that has many many features and looks nice", false),
+    
     new Idea("Search Bar", ["HTML/JS"], ["Utility", "New"], "Something that tries to search for things in a list as best as possible. Could sort websites or songs.", false),
+    
     new Idea("Tower Defense", ["HTML/JS"], ["Game", "New"], "A classic tower defense game.", false),
+
+    new Idea("Stats Game", ["HTML/JS"], ["Game", "New"], "A game with real stats. Intelligence stat limits options, arrogance stat impairs analysis.", false)
 ];
