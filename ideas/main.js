c9431debed451e500f402f21cc6d5ec8a59e152a
Bret Jones <bretcj1@gmail.com>
2022-08-04 21:40:31 -0400

    Stylistic choices

--- ideas/main.js
+++ ideas/main.js
@@ -1,24 +1,32 @@
 function createElement(idea) {
     var base = document.createElement("div");
+    base.classList.add("idea");
     var head = document.createElement("h3");
     head.innerHTML = idea.name;
 
     var type = document.createElement("div");
+    type.classList.add("typeList");
     for (let i = 0; i < idea.type.length; i++) {
         var dem = document.createElement("text");
+        dem.classList.add("type");
         dem.innerHTML = idea.type[i];
         type.appendChild(dem);
     }
 
     var tool = document.createElement("div");
+    tool.classList.add("toolList");
     for (let i = 0; i < idea.tools.length; i++) {
         var dem = document.createElement("text");
+        dem.classList.add("tool")
         dem.innerHTML = idea.tools[i];
         tool.appendChild(dem);
     }
 
-    var desc = document.createElement("text");
-    desc.innerHTML = idea.description;
+    var desc = document.createElement("div");
+    desc.classList.add("description");
+    var des = document.createElement("text");
+    des.innerHTML = idea.description;
+    desc.appendChild(des);
 
     base.append(head, type, desc, tool);
     return base;
@@ -29,3 +37,4 @@ function createElements() {
     }
 }
 createElements();
+//boxChecking();
--- main/tools.js
+++ main/tools.js
@@ -4,3 +4,10 @@ function disableDrag() {
         list[i].draggable = false;
     }
 }
+function boxChecking() {
+    var list = document.getElementsByTagName("*");
+    for (let i = 0; i < list.length; i++) {
+        list[i].style.backgroundColor = "rgba(0,255,128,0.2)";
+        list[i].style.outline = "1px solid rgb(128,0,64)";
+    }
+}
