fe3961c6fa509fd6308505e07ba1ef754cb05221
Bret Jones <bretcj1@gmail.com>
2022-08-18 13:45:29 -0400

    Messing around

---
+++ extension/main.js
@@ -0,0 +1,25 @@
+function main() {
+    var body = document.body;
+    if (body.value == undefined) {
+        body.value = 0;
+    } else {
+        body.value++;
+    }
+
+    function change() {
+        var body = document.body;
+        body.style.backgroundColor = "#0000ff";
+    }
+    function nope() {
+        var body = document.body;
+        body.style.backgroundColor = "initial";
+    }
+
+    if (body.value%2 == 0) {
+        console.log("On");
+        change();
+    } else {
+        console.log("Off");
+        nope();
+    }
+}
