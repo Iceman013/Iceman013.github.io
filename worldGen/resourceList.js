e84857fc7d3f148b847fe2b8470b6c9ef07cd6ed
Bret Jones <bretcj1@gmail.com>
2022-09-12 19:46:56 -0400

    Slight changes

--- worldGen/resourceList.js
+++ worldGen/resourceList.js
@@ -1,10 +1,10 @@
 function getResourceList() {
     var list = [];
 
-    var item = new Resource(1, "Trees", 0.02, 0.07, [2], ["images/resources/treea.png"]);
+    var item = new Resource(1, "Trees", 0.02, 0.07, [2], ["images/resources/treea.png","images/resources/treeb.png","images/resources/treec.png"]);
     list.push(item);
 
-    var item = new Resource(2, "Rock", 0.03, 0, [2,3], ["images/resources/rocka.png"]);
+    var item = new Resource(2, "Rock", 0.03, 0, [2,3], ["images/resources/rocka.png","images/resources/rockb.png","images/resources/rockc.png"]);
     list.push(item);
 
     var item = new Resource(3, "Bush", 0.02, 0.05, [2], ["images/resources/busha.png","images/resources/bushb.png","images/resources/bushc.png"]);
