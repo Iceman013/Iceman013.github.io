8cb53d2226d937ebb0330f002160a32c273efa01
Bret Jones <bretcj1@gmail.com>
2022-09-19 19:36:38 -0400

    juggling

--- juggling/content/trickList.js
+++ juggling/content/trickList.js
@@ -1,3 +1,7 @@
 const TRICKS = [
-    new Trick("Shower", ["3-Ball"], "shower.html", "This trick involves passing from one side to the other in a circular pattern."),
+    new Trick("Cascade", ["3-Ball"], "cascade.html", "This is the basic 3 ball juggling technique."),
+
+    new Trick("Reverse Cascade", ["3-Ball"], "reverse.html", "This is the basic 3 ball juggling done in reverse."),
+
+    new Trick("Shower", ["3-Ball","4-Ball","5-Ball"], "shower.html", "This trick involves passing from one side to the other in a circular pattern."),
 ];
