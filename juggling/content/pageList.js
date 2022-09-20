10b017e7ed8f8d23eb58d12c6a623d9334aa7137
Bret Jones <bretcj1@gmail.com>
2022-09-19 22:48:25 -0400

    Sure why not

--- juggling/content/pageList.js
+++ juggling/content/pageList.js
@@ -1,4 +1,5 @@
 const PAGES = [
     new Page("About", "https://iceman013.github.io/juggling/core/about.html"),
     new Page("Tricks", "https://iceman013.github.io/juggling/core/fullList.html"),
+    new Page("Buying Stuff", "https://iceman013.github.io/juggling/core/recommend.html"),
 ];
