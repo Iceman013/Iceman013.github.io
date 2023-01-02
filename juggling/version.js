fe46c1b26680cdbbe1fc15b31a4d18947bb54ba6
Bret Jones <bretcj1@gmail.com>
2023-01-01 21:12:54 -0500

    TreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEeeeeeeeee3e

--- juggling/version.js
+++ juggling/version.js
@@ -1,11 +1,11 @@
 const major = 2;
 const mediu = 6;
-const minor = 0;
+const minor = 1;
 
 /* Totals (Since 1.0.0)
  * major: 2
  * mediu: 11
- * minor: 23
+ * minor: 24
  */
 
 function update() {
