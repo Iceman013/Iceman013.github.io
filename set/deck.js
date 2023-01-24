5ac304d605238dd9f086e5cfce80602aa29af425
Bret Jones <bretcj1@gmail.com>
2023-01-24 11:18:01 -0500

    Added timer and show answer for those idiots that cant just figure it out

--- set/deck.js
+++ set/deck.js
@@ -2,11 +2,15 @@ var deck = [];
 var displayed = [];
 const pick = new Event("pick");
 
+var time = 0;
+var timer;
 document.body.onresize = function() {
     drawThem();
 }
 
 function clear() {
+    clearInterval(timer);
+
     var base = document.getElementById("board");
     while (base.firstChild) {
         base.removeChild(base.firstChild);
@@ -25,27 +29,38 @@ function clear() {
 
     base.appendChild(message);
 }
-function checkDisplay(list) {
-    var found = false;
+function getAnswer(list) {
+    var answers = [];
     for (let i = 0; i < list.length - 2; i++) {
         for (let j = i + 1; j < list.length - 1; j++) {
             for (let k = j + 1; k < list.length; k++) {
                 if (isSet(list[i].card, list[j].card, list[k].card)) {
-                    found = true;
+                    var thisAnswer = [i,j,k];
+                    answers.push(thisAnswer);
                     //console.log((i+1) + " " + (j+1) + " " + (k+1));
-                    i = list.length;
-                    j = list.length;
-                    k = list.length;
+                    // Save time or check for all answers
+                    if (true) {
+                        i = list.length;
+                        j = list.length;
+                        k = list.length;
+                    }
+                    
                 }
             }
         }
     }
-    return found;
+    return answers;
+}
+function checkDisplay(list) {
+    return (getAnswer(list).length != 0);
 }
 function replace(trio) {
     var cdis = [];
     for (let i = 0; i < displayed.length; i++) {
         if (trio.includes(displayed[i])) {
+            if (displayed[i].base.classList.contains("clue")) {
+                displayed[i].base.classList.remove("clue");
+            }
             displayed[i].card = null;
             makeCard(displayed[i].base, displayed[i].card);
             displayed[i].activate();
@@ -126,6 +141,15 @@ function drawThem() {
         makeCard(displayed[i].base, displayed[i].card);
     }
 }
+function showAnswer() {
+    var answers = getAnswer(displayed);
+    for (let i = 0; i < answers[0].length; i++) {
+        var base = displayed[answers[0][i]].base;
+        if (!base.classList.contains("clue")) {
+            base.classList.add("clue");
+        }
+    }
+}
 function start() {
     var board = document.getElementById("board");
     while (board.firstChild) {
@@ -140,13 +164,10 @@ function start() {
     var built = false;
     while (!built) {
         for (let i = 0; i < 3; i++) {
-            var row = document.createElement("div");
-            row.classList.add("row");
-            board.appendChild(row);
             for (let j = 0; j < 4; j++) {
                 var card = document.createElement("div");
                 card.id = "a" + i + "b" + j;
-                row.appendChild(card);
+                board.appendChild(card);
                 var cardpos = Math.floor(deck.length*Math.random());
                 displayed.push(new Slot(deck[cardpos], card));
                 deck.splice(cardpos, 1);
@@ -156,5 +177,12 @@ function start() {
     }
     drawThem();
     document.getElementById("remain").innerHTML = deck.length;
+
+    time = 0;
+    timer = setInterval(function() {
+        var output = document.getElementById("time");
+        output.innerHTML = Math.round(10*time)/10;
+        time += 0.1;
+    }, 100);
 }
 start();
