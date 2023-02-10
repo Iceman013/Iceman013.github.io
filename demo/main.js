67cf6ec825f042ad7fdd8fcb6c179e65f49de441
Bret Jones <bretcj1@gmail.com>
2023-02-09 21:40:34 -0500

    I am so sick of this. I hate instagram. They are evil.

--- demo/main.js
+++ demo/main.js
@@ -1,21 +1,5 @@
-function drawEdge(a, b) {
-	if (card.getShape() == "Squiggle") {
-		var path = [
-			[0, 0.2],
-			[0.25, 0],
-			[1, 0.25],
-			[0.6, 0.7],
-			[1, 0.8],
-			[0.75, 1],
-			[0, 0.8],
-			[0.4, 0.3]
-		];
-		areaGenerator = d3.line().curve(d3.curveBasisClosed);
-		area = areaGenerator(scale(path));
-	}
-
-	d3.select("g#" + id)
-		.append("path")
-		.attr("d", area)
-		.attr("class", card.getColor() + " " + card.getFill());
-}
+const RSS_URL = "https://rss.app/feeds/ahp2p8UMMzz6A8Xs.xml";
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => console.log(data))
---
+++ juggling/javascript/feed.js
@@ -0,0 +1,40 @@
+const RSS_URL = "https://rss.app/feeds/ahp2p8UMMzz6A8Xs.xml";
+
+function makeItem(item) {
+    function removeJunk(text) {
+        if (text.includes("<img src=")) {
+            var ender = ' style="width: 100%;" />';
+            text = text.substring(0, text.indexOf("<img src=")) + text.substring(text.indexOf(ender) + ender.length);
+        }
+        return text.substring(9, text.length - 3);
+    }
+
+    var base = document.createElement("div");
+    document.getElementById("insta").appendChild(base);
+
+    // Title
+    var title = document.createElement("h3");
+    title.innerHTML = removeJunk(item.querySelectorAll("title")[0].innerHTML);
+    base.appendChild(title);
+
+    var imgUrl = item.querySelectorAll("content")[0].attributes.url.nodeValue;
+    // Description
+    var description = document.createElement("div");
+    description.innerHTML = removeJunk(item.querySelectorAll("description")[0].innerHTML);
+    base.appendChild(description);
+
+    // Date
+    var date = document.createElement("p");
+    date.innerHTML = item.querySelectorAll("pubDate")[0].innerHTML;
+    base.appendChild(date);
+    //var finalUrl = "https://cors-anywhere.azm.workers.dev/" + imgUrl;
+}
+fetch(RSS_URL)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+        const items = data.querySelectorAll("item");
+        for (let i = 0; i < items.length; i++) {
+            makeItem(items[i]);
+        }
+});
