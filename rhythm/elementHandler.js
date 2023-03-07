b056ee33d37921489135658a48220560adb72ec9
Bret Jones <bretcj1@gmail.com>
2023-03-07 13:30:27 -0500

    Its a thing

--- rhythm/elementHandler.js
+++ rhythm/elementHandler.js
@@ -1,6 +1,24 @@
 function makeAudioPlayer(item) {
-    var base = document.createElement("audio");
-    base.src = "songs/" + item.src;
-    base.controls = true;
+    var base = document.createElement("div");
+
+    var title = document.createElement("h3");
+    title.innerHTML = item.name;
+    base.appendChild(title);
+
+    var artist = document.createElement("p");
+    artist.innerHTML = item.artist;
+    base.appendChild(artist);
+
+    var audio = document.createElement("audio");
+    audio.src = "songs/" + item.src;
+    audio.controls = true;
+    base.appendChild(audio);
+
     return base;
+}
+function showSongs() {
+    var base = document.getElementById("songList");
+    for (let i = 0; i < SONGS.length; i++) {
+        base.appendChild(makeAudioPlayer(SONGS[i]));
+    }
 }
