ecd3bd7ccbeef40892cbd9065158bedff0d84340
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2023-09-27 15:32:08 -0400

    Fixy faxy

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,20 @@
-const HOURS = 72;
-const CONVE = 60*60;
-var count = 0;
-var chance = 0.005;
-for (let i = 0; i < HOURS*CONVE; i++) {
-	for (let j = 0; j < 3; j++) {
-		if (Math.random() < chance) {
-			chance = 0.5*chance;
-			count++;
-		}
+function pick() {
+	let people = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+	let out = [];
+	for (let i = 0; i < 4; i++) {
+		let r = Math.floor(people.length*Math.random());
+		out.push(people[r]);
+		people.splice(r, 1);
 	}
+	let sum = 0;
+	for (let i = 0; i < out.length; i++) {
+		sum += out[i];
+	}
+	return sum;
 }
-console.log(count);
+let avg = 0;
+const SIZE = 10000;
+for (let i = 0; i < SIZE; i++) {
+	avg += pick()/SIZE;
+}
+console.log(avg);
