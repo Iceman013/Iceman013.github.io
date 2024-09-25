a1399766d9dc1f4116c7e1931bc4ac7bb8188d79
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2024-09-25 13:08:29 -0400

    Demo

--- demo/main.js
+++ demo/main.js
@@ -1,37 +1,65 @@
-import {
-	S3Client,
-	// This command supersedes the ListObjectsCommand and is the recommended way to list objects.
-	ListObjectsV2Command,
-  } from "@aws-sdk/client-s3";
-  
-  const client = new S3Client({});
-  
-  export const main = async () => {
-	const command = new ListObjectsV2Command({
-	  Bucket: "my-bucket",
-	  // The default and maximum number of keys returned is 1000. This limits it to
-	  // one for demonstration purposes.
-	  MaxKeys: 1,
-	});
-  
-	try {
-	  let isTruncated = true;
-  
-	  console.log("Your bucket contains the following objects:\n");
-	  let contents = "";
-  
-	  while (isTruncated) {
-		const { Contents, IsTruncated, NextContinuationToken } =
-		  await client.send(command);
-		const contentsList = Contents.map((c) => ` • ${c.Key}`).join("\n");
-		contents += contentsList + "\n";
-		isTruncated = IsTruncated;
-		command.input.ContinuationToken = NextContinuationToken;
-	  }
-	  console.log(contents);
-	} catch (err) {
-	  console.error(err);
+const BALLCOUNT = 6;
+const DRAWCOUNT = 3;
+function start() {
+	let balls = [];
+	for (let i = 1; i <= BALLCOUNT; i++) {
+		let item = i;
+		balls.push(item);
 	}
-  };
-  
-  
+
+	function sort(input) {
+		for (let i = 0; i < input.length; i++) {
+			let min = i;
+			for (let j = i + 1; j < input.length; j++) {
+				if (input[j] < input[min]) {
+					min = j;
+				}
+			}
+			let temp = input[i];
+			input[i] = input[min];
+			input[min] = temp;
+		}
+		return input;
+	}
+
+	let list = [];
+	for (let i = 0; i < balls.length; i++) {
+		for (let j = 0; j < balls.length; j++) {
+			for (let k = 0; k < balls.length; k++) {
+				let item = [];
+				item.push(balls[i]);
+				item.push(balls[j]);
+				item.push(balls[k]);
+				list.push(item);
+			}
+		}
+	}
+
+	let sortedList = [];
+	for (let i = 0; i < list.length; i++) {
+		sortedList.push(sort(list[i]));
+	}
+
+	let noDupeList = [];
+	for (let i = 0; i < sortedList.length; i++) {
+		let included = false;
+		for (let j = 0; j < noDupeList.length; j++) {
+			let thisOne = false;
+			for (let k = 0; k < noDupeList[j].length; k++) {
+				if (noDupeList[j][k] != sortedList[i][k]) {
+					thisOne = true;
+				}
+			}
+			if (thisOne == false) {
+				included = true;
+			}
+		}
+		if (!included) {
+			noDupeList.push(sortedList[i]);
+		}
+	}
+
+	console.log(noDupeList);
+}
+
+start();
