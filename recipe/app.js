8f9637fa4c8f910d37b34d630e003bf4c042b1d7
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2026-04-30 19:35:56 -0400

    Actually download

--- recipe/app.js
+++ recipe/app.js
@@ -1,7 +1,7 @@
 // Service worker ?
 const registerServiceWorker = async () => {
     try {
-        await navigator.serviceWorker.register('/pwa/display-standalone/sw.js', { scope: '/pwa/display-standalone/'});
+        await navigator.serviceWorker.register('/recipe/sw.js', { scope: '/recipe/'});
         console.log('Service worker registered');
     } catch (e) {
         console.log(`Registration failed: ${e}`);
